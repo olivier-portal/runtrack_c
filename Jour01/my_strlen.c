88e4596f0064d28c4aff6d1feb8ca5f6bf243dcc
MrRobot <olivier.portal@laplateforme.io>
2025-03-31 17:23:30 +0200

    feat: my_strlen

---
+++ Jour01/my_strlen.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    char greetings[] = "Hello, World!";
+    int length = strlen(greetings);
+    printf("%d\n", length);
+    return 0;
+}
