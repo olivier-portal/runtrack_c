27e9c8cab026f187b151764ba1de823d7e5b093d
MrRobot <olivier.portal@laplateforme.io>
2025-04-01 11:06:41 +0200

    feat: array_clone ok

---
+++ Jour02/array_clone.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void array_clone(char **strs, int n) {
+    char **clone = malloc(n * sizeof(char *));
+    if (clone == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        clone[i] = malloc((strlen(strs[i]) + 1) * sizeof(char));
+        strcpy(clone[i], strs[i]);
+        printf("%s\n", clone[i]);
+        free(clone[i]);
+    }
+    free(clone);
+}
+
+int main() {
+    char *strs[] = {"Hello", "World", "!"};
+    int n = sizeof(strs) / sizeof(strs[0]);
+    array_clone(strs, n);
+    return 0;
+}
+#include <stdlib.h>
